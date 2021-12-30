44338032e092580f48964b6906858996b5cfce8e
daadreyaa <daadreyaa@gmail.com>
2021-12-30 15:11:14 +0530

    minor changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:foka_app_v1/components/rounded_button.dart';
-import 'package:foka_app_v1/fluid_monitor.dart';
+import 'package:foka_app_v1/screens/fluid_monitor.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:progresso/progresso.dart';
 
