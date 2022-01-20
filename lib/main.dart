4e76ff7d2f13c07b8610ca7ef50a95305c8adb33
Akhash16 <akhash.xs@gmail.com>
2022-01-21 00:05:45 +0530

    added geofencing

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       title: "Foka Tech",
-      initialRoute: QrScreen.id,
+      initialRoute: LocationScreen.id,
       routes: {
         LoginScreen.id: (context) => const LoginScreen(),
         RegisterScreen.id: (context) => const RegisterScreen(),
